83bad28d703d6f522ce3e997b99d9d3842698291
unknown <javiervitalioperalta@gmail.com>
2024-10-09 21:06:05 -0400

    form errors

--- src/actions/reservation/get-reservation-details.action.ts
+++ src/actions/reservation/get-reservation-details.action.ts
@@ -1,27 +1,52 @@
 import { z } from "astro/zod";
 import { defineAction } from "astro:actions";
+import { error } from "node_modules/astro/dist/core/logger/core";
 
 export const getReservationDetails = defineAction({
   accept: "form",
   input: z.object({
-    checkIn: z.date(),
-    checkOut: z.date(),
+    checkIn: z.string(),
+    checkOut: z.string(),
     adults: z.number(),
     kids: z.number().optional(),
   }),
-  handler: async ({checkIn, checkOut, adults, kids}) => {
+  handler: async ({checkIn, checkOut, adults, kids}, {cookies}) => {
 
-
-    if (checkIn === null || checkOut === null) {
-        alert("Debe seleccionar la fecha correctamente");
+    try {
+      const errors: Record<string, string> = {};
+       // Validar fechas
+       if (!checkIn || !checkOut) {
+        errors.dateMissing = "Debe seleccionar las fechas correctamente.";
+      } else if (checkIn >= checkOut) {
+        errors.dateOrder = "La fecha de Check-Out no puede ser anterior a la de Check-In.";
       }
-  
-      if (checkIn >= checkOut) {
-        alert("La fecha de CheckOut no puede ser anterior a la de CheckIn");
-      }
 
-      const reservation = `CheckIn: ${checkIn} "/n" CheckOut: ${checkOut} "/n" Adultos: ${adults} "/n" Ninos: ${kids}`;
+      const reservation = {
+        checkIn,
+        checkOut,
+        adults,
+        kids: kids || 0,
+      };
 
-    return reservation;
-  },
+    // Almacenar la reserva en una cookie
+    cookies.set('reservation', JSON.stringify(reservation), {
+      // path: '/',
+      maxAge: 60 * 30 // 30 minutos
+    });
+
+    return  {
+      success: true,
+      reservation,
+    }
+    
+
+    } catch (error) {
+      return {
+        success: false,
+        errors: {
+          general: "Error al procesar la reserva.",
+        },
+      };
+    }
+  }
 });
