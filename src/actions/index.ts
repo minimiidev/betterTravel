018f6ec145c707e4b0e302ed41662e5a595ea69f
unknown <javiervitalioperalta@gmail.com>
2024-10-15 21:11:48 -0400

    astro action form

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,9 @@
-import { getReservationDetails } from './reservation/get-reservation-details.action';
+import { sendEmail } from "./resend/send-email";
+import { getReservationDetails } from "./reservation/get-reservation-details";
+
 
 
 export const server = {
+    sendEmail,
     getReservationDetails
 }
---
+++ src/actions/resend/send-email.ts
@@ -0,0 +1,48 @@
+import { z } from "astro/zod";
+import { defineAction } from "astro:actions";
+import { Resend } from 'resend';
+
+export const sendEmail = defineAction({
+  accept: "form",
+  input: z.object({
+    userName: z.string(),
+    userNumber: z.number(),
+    userEmail: z.string(),
+    userDesc: z.string(),
+    userComment: z.string().optional(),
+  }),
+  handler: async ({userDesc, userEmail,userName,userNumber,userComment}) => {
+
+    const resend = new Resend(import.meta.env.RESEND_API_KEY)
+    
+    if(!userComment) {
+      userComment = ""
+    }
+    // const subject = "It works!"
+    const html = `    <h1>De: ${userName}</h1>
+                     <h3>Telefono: ${userNumber}</h3>
+                     <h3>Correo Electronico: ${userEmail}</h3>
+                     <p>${userDesc}</p>
+                     <small>${userComment}</small> `
+    
+
+    const body = {
+      from: "Better Travel <onboarding@resend.dev>",
+      html: html,
+      subject: "Reserva",
+      to: "javiervitalioperalta@gmail.com"
+    }
+
+    console.log(body)
+
+    const { data, error } = await resend.emails.send(body)
+    if (error) {
+      return console.error({ error });
+    }
+
+    console.log(data)
+
+    return data
+
+  },
+});
