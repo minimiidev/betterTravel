441c3580cfb0d533b5c673e91b56498808c2c522
unknown <javiervitalioperalta@gmail.com>
2024-10-30 22:19:38 -0400

    hotels deleted

--- src/data.ts
+++ src/data.ts
@@ -11,10 +11,7 @@ export const links = [
     text: "Resorts",
     path: "/resorts",
   },
-  {
-    text: "Hoteles",
-    path: "/hoteles",
-  },
+  
   {
     text: "Cruceros",
     path: "/cruceros",
